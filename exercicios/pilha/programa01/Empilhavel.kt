ce1eaf6550d6be074732d149d8e0dfe1d18ef354
pedro <pedroflopes1710@gmail.com>
2024-04-04 12:15:36 -0300

    add pilha estática

---
+++ exercicios/pilha/programa01/Empilhavel.kt
@@ -0,0 +1,15 @@
+interface Empilhavel {
+    // Metodos principais
+    fun empilhar(dado: Any?)    //C
+    fun atualizar(dado: Any?)   //U
+    fun espiar(): Any?          //R
+    fun desempilhar(): Any?     //D
+    // Metodos auxiliares
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+}
+
+
+
+
---
+++ exercicios/pilha/programa01/PilhaEstatica.kt
@@ -0,0 +1,69 @@
+class PilhaEstatica : Empilhavel {
+    
+	private var ponteiroTopo: Int
+	private var dados: Array<Any?>
+	
+	constructor(tamanho: Int) {
+		dados = arrayOfNulls(tamanho)
+		ponteiroTopo = -1
+	}
+	
+	constructor() : this(10)
+
+    //Método atualizar => Update
+	override fun atualizar(dado: Any?) {
+		if (!estaVazia())
+			dados[ponteiroTopo] = dado
+		else
+			println("Stack is empty!")
+	}
+
+	override fun empilhar(dado: Any?) {
+		if (!estaCheia()) {
+			ponteiroTopo++
+			dados[ponteiroTopo] = dado
+		} else {
+			println("Stack is full!")
+		}
+	}
+	
+	override fun desempilhar(): Any? {
+		var dadoTopo: Any? = null
+		if (!estaVazia()) {
+			dadoTopo = dados[ponteiroTopo]
+			ponteiroTopo--
+		} else {
+			println("Stack is empty!")
+		}
+		return dadoTopo
+	}
+	
+	override fun espiar(): Any? {
+		var dadoTopo: Any? = null
+		if (!estaVazia()) {
+			dadoTopo = dados[ponteiroTopo]
+		} else {
+			println("Stack is empty!")
+		}
+		return dadoTopo
+	}
+	
+	override fun estaCheia(): Boolean {
+		return (ponteiroTopo == dados.size - 1)
+	}
+	
+	override fun estaVazia(): Boolean {
+		return (ponteiroTopo == -1)
+	}
+	
+	override fun imprimir(): String {
+		var resultado = "["
+		for (i in ponteiroTopo downTo 0) {
+			if (i == 0)
+				resultado += "${dados[i]}"
+			else
+				resultado += "${dados[i]}, "
+		}
+		return "$resultado]"
+	}
+}
