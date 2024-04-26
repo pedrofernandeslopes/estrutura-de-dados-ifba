833ad3b69f5768b5d929a3502857e5a113a0428f
pedro <pedroflopes1710@gmail.com>
2024-04-26 11:45:29 -0300

    added static simple queue

--- implementacoes-aula/fila/fila-estatica/fila-simples/Enfileiravel.kt
+++ implementacoes-aula/fila/fila-estatica/fila-simples/Enfileiravel.kt
@@ -1,9 +1,12 @@
 interface Enfileiravel {
-    // Metodos principais
+    //Métodos Principais
     fun enfileirar(dado: Any?)
+    fun atualizar(dado: Any?)
     fun desenfileirar(): Any?
     fun espiar(): Any?
-    // Metodos auxiliares
+
+
+    //Métodos Auxiliares
     fun estaCheia(): Boolean
     fun estaVazia(): Boolean
     fun imprimir(): String
--- implementacoes-aula/fila/fila-estatica/fila-simples/FilaEstatica.kt
+++ implementacoes-aula/fila/fila-estatica/fila-simples/FilaEstatica.kt
@@ -1,54 +1,61 @@
 class FilaEstatica(private val tamanho: Int = 10) : Enfileiravel {
- 
 	private var ponteiroInicio = 0
-	private var ponteiroFim = -1 
+	private var ponteiroFim = -1
 	private var dados: Array<Any?> = arrayOfNulls(tamanho)
-    
+
 	override fun enfileirar(dado: Any?) {
-		if (!estaCheia()) {
+		if(!estaCheia()) {
 			ponteiroFim++
 			dados[ponteiroFim] = dado
 		} else {
 			println("Queue is full!")
+		}	
+	}
+
+	override fun atualizar(dado: Any?) {
+		if(!estaVazia()) {
+			dados[ponteiroInicio] = dado
+		} else {
+			println("Queue is empty!")
 		}
 	}
-	
+
 	override fun desenfileirar(): Any? {
-		var dadoInicio: Any? = null
-		if (!estaVazia()) {
-			dadoInicio = dados[ponteiroInicio]
+		var dadoInicio: <Any?> = null
+		if(!estaVazia()) {
 			ponteiroInicio++
+			dados[ponteiroInicio] = dado
 		} else {
 			println("Queue is empty!")
 		}
 		return dadoInicio
 	}
-	
+
 	override fun espiar(): Any? {
-		var dadoInicio: Any? = null
-		if (!estaVazia()) {
+		var dadoInicio: <Any?> = null
+		if(!estaVazia()) {
 			dadoInicio = dados[ponteiroInicio]
 		} else {
-			println("Queue is empty!")
+		println("Queue is empty!")
 		}
 		return dadoInicio
 	}
-	
+
 	override fun estaCheia(): Boolean {
-		return ponteiroFim == dados.size - 1
+		return(ponteiroFim == dados.size - 1)
 	}
-	
+
 	override fun estaVazia(): Boolean {
-		return ponteiroInicio == ponteiroFim + 1
+		return(ponteiroInicio == ponteiroFim + 1)
 	}
-	
+
 	override fun imprimir(): String {
 		var resultado = "["
 		for (i in ponteiroInicio..ponteiroFim) {
 			if (i == ponteiroFim)
 				resultado += "${dados[i]}"
 			else
-				resultado += "${dados[i]},"
+				resultado += "${dados[i], }"
 		}
 		return "$resultado]"
 	}
