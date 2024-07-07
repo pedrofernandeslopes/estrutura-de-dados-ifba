bdcb59acf16c945348c07e2391dd9ef4797a7c14
pedro <pedroflopes1710@gmail.com>
2024-07-07 14:41:23 -0300

    Fila estática simples

---
+++ src/fila/estatica/simples/Enfileiravel.kt
@@ -0,0 +1,12 @@
+interface Enfileiravel {
+    //Métodos Principais
+    fun enfileirar(dado: Any?)
+    fun atualizar(dado: Any?)
+    fun desenfileirar(): Any?
+    fun frente(): Any?
+
+    //Métodos Auxiliares
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+}
